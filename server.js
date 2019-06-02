7366e763cb2a201a97be524423fe92b2ffce31ef
ego <ego@hackermail.com>
2019-06-02 17:02:30 +0630

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    bodyParser      = require('body-parser'),
+    request = require('sync-request'),
+    api        = require('./routes')
+
+var app = express();
+
+app.get('/posts', api.posts);
+app.get('/posts/:id', api.post);
+
+
+
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+    console.log(process.env.skipUrl);
+    console.log(process.env.allMovie);
+    console.log('Express server listening on port ' + server.address().port);
+});
